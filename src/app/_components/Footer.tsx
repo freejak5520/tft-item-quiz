0b5d9be61050a211a69f1a80bbe0574d58e1a56c
Jake Lee <freejak5520@gmail.com>
2024-04-20 15:43:02 +0900

    푸터에 제작자 정보 표시

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -1,6 +1,18 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <div className="h-96 border-t border-border-300 bg-bg-200 dark:border-border-700 dark:bg-bg-800"></div>
+    <div className="h-96 border-t border-border-300 bg-bg-200 p-8 text-center dark:border-border-700 dark:bg-bg-800">
+      Made by Jake Lee{" "}
+      <Link
+        className="font-bold text-blue-500 underline"
+        href="https://github.com/freejak5520"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Github
+      </Link>
+    </div>
   );
 };
 
